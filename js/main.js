cabe8e20c882864f656e4d0ed73b42d480d3ffad
Charlotte Keddie <charlottekeddie@gmail.com>
2020-04-02 11:30:14 +0200

    search function and css media queries complete

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-$('.light').light();
+const search = document.getElementById('searchbox');
+const caption = document.querySelectorAll('.light');
+
+search.addEventListener('keyup', function() {
+    const searchTerm = search.value.toLowerCase();
+    for (let i = 0; i < caption.length; i ++) {
+        const searchedPhoto = caption[i].getAttribute('data-title');
+        if (searchedPhoto.toLowerCase().includes(searchTerm)) {
+            caption[i].style.display = "block";
+        }   else { 
+            caption[i].style.display = "none";
+        }
+    }
+    });
+
+   
