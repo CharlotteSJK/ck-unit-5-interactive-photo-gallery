3a39d14b74c12c112e0e70ba72bed2572563d8d0
Charlotte Keddie <charlottekeddie@gmail.com>
2020-04-02 18:19:14 +0200

    Added comments

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-const search = document.getElementById('searchbox');
-const caption = document.querySelectorAll('.light');
+const search = document.getElementById('searchbox');                        // Targets the search input from user
+const caption = document.querySelectorAll('.light');                        // Targets the photo captions
 
 search.addEventListener('keyup', function() {
-    const searchTerm = search.value.toLowerCase();
+    const searchTerm = search.value.toLowerCase();                          // Ensures search is not case sensitive
     for (let i = 0; i < caption.length; i ++) {
-        const searchedPhoto = caption[i].getAttribute('data-title');
-        if (searchedPhoto.toLowerCase().includes(searchTerm)) {
-            caption[i].style.display = "block";
+        const searchedPhoto = caption[i].getAttribute('data-title');        
+        if (searchedPhoto.toLowerCase().includes(searchTerm)) {                 
+            caption[i].style.display = "block";                             // If the searched term is included in the caption show the photo
         }   else { 
-            caption[i].style.display = "none";
+            caption[i].style.display = "none";                              // If the searched term is included in the caption hide the photo
         }
     }
     });
